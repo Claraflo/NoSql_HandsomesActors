95e4d59218d2772d1674c5fe46b12e8e07d7fddd
Claraflo <clara.florac@gmail.com>
2021-04-22 02:08:21 +0200

    ajout complement dans description technique + finalisation scrpit mongoConsult

--- mongoConsult.py
+++ mongoConsult.py
@@ -2,12 +2,14 @@ import pandas as pd
 from pymongo import MongoClient
 import json
 import os
+import pprint
 
-
+#Connextion a la base
 client = MongoClient('127.0.0.1:27017')
 db = client['projectActors']
 col = db['movies']
 
+#Affichage des statistiques
 print('**********************')
 print('Connexion a mongo')
 print('Noms des bases presentes : '+', '.join(client.list_database_names()))
@@ -16,25 +18,29 @@ print('Stat db : ',db.command("dbstats"))
 print('**********************\n')
 
 
-#Affiche toute la base
-#cursor = col.find({})
-#for document in cursor:
-#	print(document)
-
-
+# Compte le nombre de colonnes enregistrees
 document = col.count_documents({})
-print(document,'lignes sont enregistrees')
+print(document,'colonnes sont enregistrees.')
 
-#document = col.aggregate([{"$group" : {"_id" : "$title"}}])
-
+# Affichage du nombre de films presents reellement en eliminant les doublons
 document = col.distinct("title") 
 count = len(document)
+print('Il y a',count,'films presents.')
 
-print('Il y a '+str(count)+' films.')
-#for cursor in document:
-#	print(cursor)
 
-res = col.find({'title':'Once Upon a Time In Hollywood'})
+#Suppression des doublons
+cursor = col.aggregate([
+	{"$group" : {"_id" : "$title", "unique_ids": {"$addToSet": "$_id"},"count":{"$sum":1}}},
+	{"$match": {"count": { "$gte": 2 }}}					
+	])
 
-for i in res :
-	print(i)
+response = []
+for doc in cursor:
+    del doc["unique_ids"][0]
+    for id in doc["unique_ids"]:
+        response.append(id)
+
+col.delete_many({"_id": {"$in": response}})
+
+
+
